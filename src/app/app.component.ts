81ba7bebfce7c41aad584e70fe836fa00e8a5c6a
Castaño Codesal <nacc@gft.com>
2023-10-23 09:02:11 +0200

    add piano project

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  
+
+  
+  
+  aplicarSonido(number:number){
+    const audio = new Audio();
+    audio.src = '../assets/sonidos/note' + number + '.wav';
+    audio.load();
+    audio.play();
+  }
+}
